dc3bf70ff4d8622ecddc7b8219964259ed118d5c
daveklmn <63654619+daveklmn@users.noreply.github.com>
2020-05-05 13:50:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            Hello World. Changes Made in Dev Tenant 
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
